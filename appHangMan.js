9017bb6fd43e8031ecf8a08b8158384b7ba614fd
Sylvie-B <unsolo@hotmail.fr>
2021-12-06 16:33:19 +0100

    create hollow box

--- appHangMan.js
+++ appHangMan.js
@@ -15,10 +15,12 @@ let wordArray = [
 ];
 
 let letterBtn = document.getElementById('letter').getElementsByTagName('a');
-
+let word = document.getElementById('word');
 // beginning
 // shake array
 shakeArray();
+// create div on word
+hollowWord(wordArray[0]);
 
 for (let i = 0 ; i < letterBtn.length ; i++){
     letterBtn[i].addEventListener('click', function (event){
@@ -38,7 +40,16 @@ function shakeArray (){
         let x = Math.floor(Math.random() * wordArray.length);
         wordArray[i] = wordArray[x];
         wordArray[x] = item;
+    }
+}
 
+function hollowWord (theWord){
+    for(let char of theWord){
+        let box = document.createElement('div');
+        box.style.borderBottom = "3px solid black";
+        box.style.height = '5vh';
+        box.style.width = '2.5vw';
+        word.appendChild(box);
     }
 }
 
