5929a30a41277039002f8a2531f56f15d7d88699
Sylvie-B <unsolo@hotmail.fr>
2021-12-06 23:51:35 +0100

    create start -> restart

--- appHangMan.js
+++ appHangMan.js
@@ -14,49 +14,55 @@ let wordArray = [
     "LANGAGE",
 ];
 
+let startBtn = document.querySelector('#startBtn');
 let letterBtn = document.getElementsByClassName('letter');
 let word = document.getElementById('word');
 let ltrWord = document.getElementById("word").getElementsByTagName("div");
 let wrongLetter = document.getElementById('wrongLetter');
 
-// beginning
-
-// shake array
-shakeArray();
-
-// create div with first word
-hollowWord(wordArray[0]);
-// console.log(wordArray[0]);
-let game = 0;
 
 let count = 0;
 
-for (let l = 0 ; l < letterBtn.length ; l++){
-    letterBtn[l].addEventListener('click', function (event){
-        event.preventDefault();
+// beginning
+startBtn.addEventListener('click', function (event){
+    event.preventDefault();
+    let game = 0;
+    startBtn.innerHTML = "Restart";
+    // shake array
+    shakeArray();
 
-        // recup word
+    if(game < wordArray.length){
         let theWord = wordArray[game];
+        console.log(theWord)
+        // create div with this word
+        hollowWord(theWord);
+    }
 
-        // recup letter
-        let ltr = this.innerHTML;
+})
 
-        // search in word (as array)
-        if(theWord.includes(ltr)){
-            for(let c = 0 ; c < theWord.length ; c++){
-                if( ltr === theWord[c]){
-                    ltrWord[c].innerHTML = ltr;
-                }
-            }
-        }
-        else {
-            let wrongLtr = document.createElement('div');
-            wrongLtr.innerHTML = ltr;
-            wrongLetter.appendChild(wrongLtr);
-        }
 
-    })
-}
+//         // recup word
+//         let theWord = wordArray[game];
+//
+//         // recup letter
+//         let ltr = this.innerHTML;
+//
+//         // search in word (as array)
+//         if(theWord.includes(ltr)){
+//             for(let c = 0 ; c < theWord.length ; c++){
+//                 if( ltr === theWord[c]){
+//                     ltrWord[c].innerHTML = ltr;
+//                 }
+//             }
+//         }
+//         else {
+//             let wrongLtr = document.createElement('div');
+//             wrongLtr.innerHTML = ltr;
+//             wrongLetter.appendChild(wrongLtr);
+//         }
+//
+//     })
+// }
 
 // create function
 function shakeArray (){
@@ -69,11 +75,19 @@ function shakeArray (){
 }
 
 function hollowWord (theWord){
+    word.innerHTML = "";
     for(let char of theWord){
         let box = document.createElement('div');
-
         word.appendChild(box);
     }
 }
 
-
+function getLetter () {
+    for (let l = 0 ; l < letterBtn.length ; l++){
+        letterBtn[l].addEventListener('click', function (event) {
+            event.preventDefault();
+            letterBtn[l].className = "used";
+            return letterBtn[l];
+        })
+    }
+}
