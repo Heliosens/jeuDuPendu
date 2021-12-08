63cd0efbf0bf4f670bd1cc6797eba4bc0c40543d
Sylvie-B <unsolo@hotmail.fr>
2021-12-08 19:16:18 +0100

    redo js

--- appHangMan.js
+++ appHangMan.js
@@ -12,7 +12,7 @@ let arrayOfWord = [
     "ARTISTE",
     "DEVELOPPEUR",
     "LANGAGE",
-    "ALIGATOR",
+    "ALLIGATOR",
     "GATEAU",
     "ORANGE",
     "ECHARPE",
@@ -27,6 +27,12 @@ let arrayOfWord = [
     "NATATION",
     "ARCHERIE",
     "ANIMATION",
+    "AVIATEUR",
+    "DOUZAINE",
+    "PAPILLON",
+    "ORCHIDEE",
+    "KIWI",
+    "WAGON",
 ];
 
 // get elements
@@ -37,12 +43,15 @@ const alphaB = document.getElementById('letter').getElementsByTagName('a');
 
 // get target image
 const hangImg = document.getElementById('hangMan');
+hangImg.style.backgroundImage = "url('img/pendu-10.png')";
 
 // get target word
 const wordPlace = document.getElementById('word');
-
+const letterPlace = wordPlace.getElementsByTagName('div');
 // get start button
 const start = document.getElementById('startBtn');
+const restart = document.getElementById('restart');
+restart.style.display = "none";
 
 // get info 0 & 1
 const info = document.getElementById('btn').getElementsByTagName('span');
@@ -50,11 +59,63 @@ const info = document.getElementById('btn').getElementsByTagName('span');
 // get wrong side
 const wrongLetter = document.getElementById('wrongLetter');
 
+let win = 0;
 // start
 start.addEventListener('click', function (event){
-    event.preventDefault();
+    hangImg.style.backgroundImage = "url('img/pendu-0.png')";
 
-    let word = arrayOfWord[Math.floor( Math.random() * arrayOfWord.length)];
+    // get random word
+    const word = arrayOfWord[Math.floor( Math.random() * arrayOfWord.length)];
 
+    // create letter place
+    for(let char of word){
+        wordPlace.appendChild(document.createElement('div'));
+    }
 
+    // allowed letters access
+    sideFrame.style.display = "none";
+    start.style.display = "none";
+    restart.style.display = "unset";
+
+    // play
+    for(let item of alphaB){
+        item.addEventListener('click', function (event){
+            event.preventDefault();
+
+            let ltr = this.innerHTML;
+            console.log(ltr);
+
+            this.style.visibility = "hidden";
+
+            // test letter
+            if(word.includes(ltr)){                                 // if the letter is in the word
+                for(let char = 0 ; char < word.length ; char++){
+                    if(word[char] === ltr){                         // find each index
+                        letterPlace[char].innerHTML = ltr;
+                        letterPlace[char].style.border = 'none';
+                        win++;
+                        if(win === word.length){
+                            info[0].innerHTML = "Gagné !!!";
+                            sideFrame.style.display = "unset";
+
+                        }
+                    }
+                }
+            }
+            else {
+                let wrongLtr = document.createElement('div');
+                wrongLtr.innerHTML = ltr;
+                wrongLetter.appendChild(wrongLtr);
+                let w = wrongLetter.getElementsByTagName('div').length;
+                hangImg.style.backgroundImage = "url('img/pendu-" + w + ".png')";
+                if(w === 10){
+                    info[0].innerHTML = "Perdu !!!";
+                    sideFrame.style.display = "unset";
+                    info[1].innerHTML = "Le mot était " + word.toLowerCase();
+                }
+            }
+        })
+    }
 })
+
+restart.addEventListener('click', ()=> location.reload());
