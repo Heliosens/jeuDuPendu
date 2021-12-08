6d87803ff17932bd46f78315818c03062e78076b
Sylvie-B <unsolo@hotmail.fr>
2021-12-08 14:43:42 +0100

    restart js

--- appHangMan.js
+++ appHangMan.js
@@ -1,5 +1,5 @@
 // array of word
-let wordArray = [
+let arrayOfWord = [
     "VOYAGE",
     "YOGA",
     "VOITURE",
@@ -29,111 +29,32 @@ let wordArray = [
     "ANIMATION",
 ];
 
+// get elements
+const sideFrame = document.getElementById('sideFrame');
+
+// get letters
+const alphaB = document.getElementById('letter').getElementsByTagName('a');
+
+// get target image
+const hangImg = document.getElementById('hangMan');
+
+// get target word
+const wordPlace = document.getElementById('word');
+
 // get start button
-let startBtn = document.getElementById('startBtn');
+const start = document.getElementById('startBtn');
 
-// get all letters, target of the word, each letter of the word, target of wrong letter
-let letterBtn = document.getElementsByClassName('letter');
-let word = document.getElementById('word');
-let ltrWord = document.getElementById("word").getElementsByTagName("div");
-let wrongLetter = document.getElementById('wrongLetter');
+// get info 0 & 1
+const info = document.getElementById('btn').getElementsByTagName('span');
 
-// get hangman
-let frame = document.querySelector('#hangMan');
-frame.style.backgroundImage = 'url("img/pendu-10.png")';
+// get wrong side
+const wrongLetter = document.getElementById('wrongLetter');
 
-// get display chance
-let info = document.getElementById('btn').getElementsByTagName('span');
-
-
-// start state
-// let count = 0;  // good letter
-
-// start or restart
-startBtn.addEventListener('click', function (event) {
+// start
+start.addEventListener('click', function (event){
     event.preventDefault();
 
-    // hide start btn
-    startBtn.parentElement.style.display = 'none';
-
-    // the word
-    const theWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    let word = arrayOfWord[Math.floor( Math.random() * arrayOfWord.length)];
 
 
-    console.log("word = " + theWord);
-
-
-    // create div with this word
-    word.innerHTML = "";
-    for(let char of theWord){
-        let box = document.createElement('div');
-        word.appendChild(box);
-    }
-
-    // no hang man
-    frame.style.backgroundImage = 'url("img/pendu-0.png")';
-    // reset chance
-    info[0].innerHTML = "10 chances";
-    info[1].innerHTML = ""
-
-    // reset visibility
-    for(let item of letterBtn){
-        item.style.visibility = "visible";
-    }
-
-    // reset wrong div
-    wrongLetter.innerHTML = "";
-
-    let count = 0;
-    // listen letters and test letter
-    let ltr = "";
-
-    for (let letter of letterBtn){
-        letter.addEventListener('click', function (event) {
-            let test = theWord;
-            console.log("test =" + test);
-            event.preventDefault();
-            // test the letter
-            ltr = letter.innerHTML;
-            letter.style.visibility = 'hidden';
-
-
-
-
-            if(test.includes(ltr)){
-
-                for(let i = 0 ; i < theWord.length ; i++){
-                    if( ltr === theWord[i]){
-                        ltrWord[i].innerHTML = ltr;
-                        ltrWord[i].style.border = 'none';
-                        count++;
-                    }
-
-                }
-                console.log('ici = ' + theWord);
-                console.log('ici test =' + test);
-                console.log("count = " + count);
-
-                if(count === theWord.length){
-                    info[1].innerHTML = "Gagné";
-
-                    // hide start btn
-                    startBtn.parentElement.style.display = 'flex';
-                }
-            }
-            else {
-                let w = wrongLetter.getElementsByTagName('div').length;
-                let wrongLtr = document.createElement('div');
-                wrongLtr.innerHTML = ltr;
-                wrongLetter.appendChild(wrongLtr);
-                frame.style.backgroundImage = 'url("img/pendu-' + w + '.png")';
-                info[0].innerHTML = (parseInt(info[0].innerHTML) - 1) + " chances";
-                if(w === 10){
-                    alert("perdu");
-
-                }
-            }
-        })
-    }
 })
-
